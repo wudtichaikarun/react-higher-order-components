be96a29dbcd4be83d763ab5d41185f2ac7da331a
Romantic <595159090004@dpu.ac.th>
2018-02-07 16:22:18 +0700

    Higher Order Components: State Management

--- src/App.js
+++ src/App.js
@@ -27,28 +27,66 @@ function logProps(WrappedComponent) {
       return <WrappedComponent {...this.props} />
     }
   }
-
-  // Inheritance Inversion
-  /*return class extends WrappedComponent {
-    componentWillReceiveProps(nextProps) {
-      console.log('Prev Props', this.props)
-      console.log('Next Props', nextProps)
-    }
-
-    render() {
-      return super.render() 
-    }
-  }*/
 }
 
-// Nomal stateless fun. component
-const ProtectedComponent = ({ auth }) => (
-  <h2>Protected Content: {auth.isLogin.toString()}</h2>
-)
+// API MOCKUP
+function fetchApi(endpoint) {
+  return new Promise((resolve, reject) => {
+    if(!endpoint) return reject(new Error('Endpoint is required.'))
 
-// Higher Order Component
-const EnhancedComponent = logProps(forAuth(ProtectedComponent))
+    return resolve({
+      articles: [
+        {id: 1, title: 'Article#1'},
+        {id: 2, title: 'Article#2'},
+        {id: 3, title: 'Article#3'}
+      ]
+    })
+  })
+}
 
+function fetchData(WrappedComponent) {
+  return class extends Component {
+    state = {
+      fetchData: {}
+    }
+
+    componentDidMount() {
+      fetchApi(WrappedComponent.API_ENDPOINT)
+        .then(fetchData => this.setState({ fetchData }))
+        .catch(err => console.log(err.message))
+    }
+    render() {
+      return <WrappedComponent {...this.props } {...this.state }/>
+    }
+  }
+}
+
+// Component ------------------------------------
+class ProtectedComponent extends Component {
+  static API_ENDPOINT = '/articles'
+
+  state = {
+    fetchData: {}
+  }
+
+  render() {
+    const { fetchData: { articles }} = this.props
+
+    return (
+      <ul>
+        {
+          articles && articles.map( ({ id, title }) =>
+            <li key={id}>{title}</li> )
+        }
+      </ul>
+    )
+  }
+}
+
+// Component (Higher Order Component)------------
+const EnhancedComponent = logProps(forAuth(fetchData(ProtectedComponent)))
+
+// Component ------------------------------------
 class App extends Component {
 
   state = {
